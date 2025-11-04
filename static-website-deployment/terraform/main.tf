b4433cd9ae9e71273c07cf668a7e75ebce9c8bb1
Global Mobarak <mh.ice.iu@gmail.com>
2025-11-04 19:59:21 +0600

    updated static website tf files

--- static-website-deployment/terraform/main.tf
+++ static-website-deployment/terraform/main.tf
@@ -2,20 +2,9 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-resource "tls_private_key" "rsa_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
 resource "aws_key_pair" "ssh_key" {
   key_name   = "nginx-server.pub"
-  public_key = tls_private_key.rsa_key.public_key_openssh
-}
-
-resource "local_file" "ssh_private_key" {
-  content         = tls_private_key.rsa_key.private_key_pem
-  filename        = "../server-key.pem"
-  file_permission = "0400"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
 resource "aws_instance" "ec2" {
@@ -35,7 +24,7 @@ ${aws_instance.ec2.public_ip}
 
 [all:vars]
 ansible_user=ubuntu
-ansible_ssh_private_key_file=${local_file.ssh_private_key.filename}
+ansible_ssh_private_key_file=~/.ssh/id_rsa
 ansible_ssh_common_args='-o StrictHostKeyChecking=no'
 EOT
 
@@ -47,6 +36,6 @@ output "ec2_info" {
   value = {
     public_ip          = aws_instance.ec2.public_ip
     private_ip         = aws_instance.ec2.private_ip
-    command_to_connect = "ssh -i ${local_file.ssh_private_key.filename} ubuntu@${aws_instance.ec2.public_ip}"
+    command_to_connect = "ssh ubuntu@${aws_instance.ec2.public_ip}"
   }
 }
